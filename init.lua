29fd2de212f3d49f1d42e1900b2aac21f9adcb98
Apipol Niyomsak <contact@apipoln.com>
2025-02-21 23:43:40 +0700

    add go lsp and plugins

--- init.lua
+++ init.lua
@@ -688,6 +688,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'typescript-language-server',
+        'gopls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
